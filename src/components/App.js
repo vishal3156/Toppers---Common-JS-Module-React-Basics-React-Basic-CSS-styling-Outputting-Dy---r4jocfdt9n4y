f04b3e542f4d5af6b2da09dc6978125f3e4b0542
Vishal Suman <119152809+vishal3156@users.noreply.github.com>
2023-04-04 15:43:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -41,31 +41,25 @@ class App extends Component {
     render() {
     	return(
     		<div id="main">
-    			{
-    				//Correct the percentage conditional for correct output
-    				this.state.list.map(function(item,index){
-				   		if(item.percent > 0)
-				   		{
-				   			return(
-				   				//Conditional statement in the className of below div to give callName="bg-pink"
-				   				//if the percent >= 90 else className = "" 
-				   				<div key={index} className={}>
-					   				<div className="name">
-					   					 //Write code to display name
-					   				</div>
-					   				<div className="percent">
-					   					 //Write code to display percentage upto 2 decimal places
-					   				</div>
-				   				</div>
-				   				);
-				   		}
-			       })
-			    }
-    		</div>
-    		);
-    }
+    			{this.state.list.map(function (item, index) {
+					if (item.percent >= 75) {
+						return (
+							<div key={index} className={item.percent >= 90 ? "bg-pink" : ""}>
+								<div className="name">
+									{item.name}
+								</div>
+								<div className="percent">
+									{(item.percent).toFixed(2)}%
+								</div>
+							</div>
+						);
+					} else {
+						return null;
+					}
+				})}
+			</div>
+		);
+	}
 }
-
-
 export default App;
 
